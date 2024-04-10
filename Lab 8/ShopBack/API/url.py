4bffdced4d0a1117ff21ced0ea03fb847a5ebdc9
DosbolBolat <dosbolbolatuly@gmail.com>
2024-04-10 20:53:07 +0500

    lab 9

--- Lab 8/ShopBack/API/url.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, re_path
-from .views import hello, about, product
-
-urlpatterns = [
-    path('hi/', hello),
-    path('about/', about),
-    path('product/', product)
-]
